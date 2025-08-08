6e40a3ff50708c69b2660df6ec54784673febe9f
komkem <komkem.contact@gmail.com>
2025-08-08 13:46:46 +0700

    fix : [api]

--- api/index.go
+++ api/index.go
@@ -214,7 +214,8 @@ func setupRoutes() {
 		protected.POST("/tasks", taskController.CreateTask)       // Create
 		protected.GET("/tasks", taskController.GetTasks)          // Read (list)
 		protected.GET("/tasks/:id", taskController.GetTask)       // Read (single)
-		protected.PATCH("/tasks/:id", taskController.UpdateTask)  // Update
+		protected.PATCH("/tasks/:id", taskController.UpdateTask)  // Update (partial)
+		protected.PUT("/tasks/:id", taskController.UpdateTask)    // Update (full)
 		protected.DELETE("/tasks/:id", taskController.DeleteTask) // Delete
 
 		// Task specific actions
